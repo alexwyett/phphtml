73361971388125f7b323e2b5fec9c4dddcccf478
Alex Wyett <alex.wyett@gmail.com>
2014-10-31 11:51:39 +0000

    Added dd, dl and dt elements.

---
+++ src/aw/html/element/Dd.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Dd element
+ *
+ * PHP Version 5.4
+ *
+ * @category  PHPHtml
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2014 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+
+namespace aw\html\element;
+
+/**
+ * Dd element
+ *
+ * PHP Version 5.4
+ *
+ * @category  PHPHtml
+ * @package   AW
+ * @author    Alex Wyett <alex@wyett.co.uk>
+ * @copyright 2014 Alex Wyett
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://www.github.com/alexwyett
+ */
+class Dd extends \aw\html\base\TextElement
+{
+    
+}
